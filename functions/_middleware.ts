8a1b2065a422ca9ed02eae1d1eb8f54a234a45a5
febkosq8 <febkosq8@yahoo.com>
2023-12-19 13:34:53 +0000

    better handler

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,14 +1,11 @@
 export const onRequest: PagesFunction = async (context) => {
 	const request = context.request;
 	console.log(`Incoming request to ${request.url}`);
-	//create a new URL object using the request url
 	try {
 		const url = new URL(request.url);
 		const path = url.pathname;
-		console.log(`Path detected : ${path}`);
-		// Check if the request is to the /api path
 		if (path.startsWith("/api")) {
-			console.log(`Proxying request : ${request.url.pathname} to API server`);
+			console.log(`Proxying request : ${path} to API server`);
 			// Proxy the request to your backend API server
 			const apiUrl = "https://serve.febkosq8.me";
 			const newUrl = new URL(request.url);
@@ -20,7 +17,9 @@ export const onRequest: PagesFunction = async (context) => {
 
 			return fetch(modifiedRequest);
 		}
-	} catch (e) {}
+		return await context.next();
+	} catch (e) {
+		return await context.next();
+	}
 	// If it's not an API request, let the request proceed without modification
-	return fetch(request);
 };
