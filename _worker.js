4dceec1f4c185407ec5eec4c2e81434a4c54b5ec
febkosq8 <febkosq8@yahoo.com>
2023-12-19 12:54:45 +0000

    add function ?

---
+++ _worker.js
@@ -0,0 +1,22 @@
+addEventListener("fetch", (event) => {
+	event.respondWith(handleRequest(event.request));
+});
+
+async function handleRequest(request) {
+	// Check if the request is to the /api path
+	if (request.url.pathname.startsWith("/api")) {
+		// Proxy the request to your backend API server
+		const apiUrl = "https://serve.febkosq8.me";
+		const newUrl = new URL(request.url);
+		newUrl.host = new URL(apiUrl).host;
+		const modifiedRequest = new Request(newUrl, request);
+
+		// Optionally, you can modify headers as needed
+		modifiedRequest.headers.set("x-request-ip", request.headers.get("cf-connecting-ip"));
+
+		return fetch(modifiedRequest);
+	}
+
+	// If it's not an API request, proceed with the regular request handling
+	return fetch(request);
+}
