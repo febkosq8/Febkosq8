b6737776b8e2750657d4bc663ad86931ed2ae2ae
febkosq8 <febkosq8@yahoo.com>
2023-12-20 18:08:41 +0000

    Fix menu single item

--- src/components/febkos/Header.tsx
+++ src/components/febkos/Header.tsx
@@ -1,17 +1,32 @@
-import Menu from "@feb/components/ui/Menu";
+import LinkPill from "@feb/components/ui/LinkPill";
 import ThemeSwitcher from "@feb/components/ui/ThemeSwitcher";
+import { useEffect, useState } from "react";
 
 export default function Header() {
+	const [currHref, setCurrHref] = useState(window.location.hash);
+	useEffect(() => {
+		window.addEventListener("hashchange", () => {
+			setCurrHref(window.location.hash);
+		});
+	}, []);
 	return (
 		<div className="flex text-2xl items-center w-full justify-between px-16 h-16 bg-background border-b-2 sticky top-0">
 			<a href="/" className="font-bold">
 				Febkosq8
 			</a>
 			<div className="flex items-center gap-4">
-				<Menu href="#about" buttonContent={"About"} />
-				<Menu href="#skillset" buttonContent={"Skillset"} />
-				<Menu href="#projects" buttonContent={"Projects"} />
-				<Menu href="#socials" buttonContent={"Socials"} />
+				<LinkPill active={currHref === "#about"} href="#about">
+					About
+				</LinkPill>
+				<LinkPill active={currHref === "#skillset"} href="#skillset">
+					Skillset
+				</LinkPill>
+				<LinkPill active={currHref === "#projects"} href="#projects">
+					Projects
+				</LinkPill>
+				<LinkPill active={currHref === "#socials"} href="#socials">
+					Socials
+				</LinkPill>
 				<ThemeSwitcher />
 			</div>
 		</div>
