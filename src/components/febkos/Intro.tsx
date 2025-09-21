971918c5e63811bcc593d16ba1b3f875272a0c70
febkosq8 <febkosq8@yahoo.com>
2025-09-21 16:04:26 +0100

    Update text size in Intro component for better readability

--- src/components/febkos/Intro.tsx
+++ src/components/febkos/Intro.tsx
@@ -1,7 +1,7 @@
 export default function Intro() {
 	return (
 		<div
-			className="intro flex min-h-(--spacing-fit-screen) flex-col items-center justify-center p-5 text-center text-5xl whitespace-pre-wrap"
+			className="intro flex min-h-(--spacing-fit-screen) flex-col items-center justify-center p-5 text-center text-3xl whitespace-pre-wrap lg:text-5xl"
 			id="intro"
 		>
 			<p>Hello</p>
