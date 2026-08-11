1c9f7a1458ce9a4c90a2dd564df7a0b585a1cfe2
febkosq8 <febkosq8@yahoo.com>
2026-08-11 19:45:01 +0100

    Refactor code structure for improved readability and maintainability

--- vite.config.js
+++ vite.config.js
@@ -2,23 +2,23 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 export default defineConfig({
-    server: {
-        port: 3000,
-        proxy: {
-            "^/api/(.*)$": {
-                target: "http://localhost:3030",
-                secure: false,
-                changeOrigin: true,
-            },
-        },
-    },
-    build: {
-        outDir: "build",
-    },
-    resolve: {
-        alias: {
-            "@feb": "/src",
-        },
-    },
-    plugins: [react(), tailwindcss()],
+	server: {
+		port: 3000,
+		proxy: {
+			"^/api/(.*)$": {
+				target: "http://localhost:3030",
+				secure: false,
+				changeOrigin: true,
+			},
+		},
+	},
+	build: {
+		outDir: "build",
+	},
+	resolve: {
+		alias: {
+			"@feb": "/src",
+		},
+	},
+	plugins: [react(), tailwindcss()],
 });
