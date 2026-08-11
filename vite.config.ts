495aab7b519b1ab355d7e4643e9b11d9bb62d55c
febkosq8 <febkosq8@yahoo.com>
2026-08-11 20:02:26 +0100

    fix: update Node version to 24.16.0 and remove proxy configuration from vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,6 @@ import { defineConfig } from "vite";
 export default defineConfig({
 	server: {
 		port: 3000,
-		proxy: {
-			"^/api/(.*)$": {
-				target: "http://localhost:3030",
-				secure: false,
-				changeOrigin: true,
-			},
-		},
 	},
 	build: {
 		outDir: "build",
